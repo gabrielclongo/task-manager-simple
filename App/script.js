9280b31ea70d3c95b53bc278e288e033b045af67
gabriellongo <gabrielcinzas@gmail.com>
2026-02-04 15:04:20 +0000

    Add working frontend and backend code

---
+++ App/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("completeTaskBtn");
+
+  button.addEventListener("click", function () {
+    alert("Task completed! Notification sent.");
+  });
+});
